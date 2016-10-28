2c9b97b9fc1eecc52a1df97d7ed7128e1a864c01
jack-nie <hust.ud.hui@gmail.com>
2016-10-29 01:33:24 +0800

    add more test

--- spec/circuit_breaker_spec.rb
+++ spec/circuit_breaker_spec.rb
@@ -16,6 +16,7 @@ describe CircuitBreaker do
 
   it 'has a version number' do
     expect(CircuitBreaker::VERSION).not_to be nil
+    expect(CircuitBreaker::VERSION).to eq "0.1.0"
   end
 
   context "when closed" do
